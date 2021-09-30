4d49d52686cd53d8dee6778f7f97f8009c3dd0c3
Melissa Keller <melissa.keller23@gmail.com>
2021-09-29 20:44:15 -0500

    added function to clean up the repetative code, output still not how I'd like it.

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,71 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+## Code Re-vamp with function
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+def melon_delivery(file_name):
+    """Input Uber-Melon Delivery File, prints it in a prettier form"""
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    the_file = open(file_name)
+    prettier_version = []
+    for line in the_file:
+        line = line.rstrip()
+        word = line.split('|')
 
+        melon = word[0]
+        count = word[1]
+        amount = word[2]
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        prettier_version.append(f'Delivered {count} {melon}s for a total of {amount}.')
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    return prettier_version
 
 
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+files = ['um-deliveries-20140519.txt', 'um-deliveries-20140520.txt', 'um-deliveries-20140521.txt']
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+day = 1
+for one_file in files:
+    print(f'Day {day}.')
+    print(melon_delivery(one_file))
+    day += 1
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+
+## Initial Code
+
+# print("Day 1")
+# the_file = open("um-deliveries-20140519.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 2")
+# the_file = open("um-deliveries-20140520.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-20140521.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
